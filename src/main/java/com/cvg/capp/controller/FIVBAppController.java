bbabc9d4fd18bde6e835a71d0d2bce1c86bcfa21
VolleyTKE <chris.a.carr@gmail.com>
2018-11-09 15:04:40 -0600

    Update pom, JSP views now display appropriately

--- src/main/java/com/cvg/capp/controller/FIVBAppController.java
+++ src/main/java/com/cvg/capp/controller/FIVBAppController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author ccarr
  */
 
-@RestController
+@Controller
 public class FIVBAppController {
 
     @RequestMapping("/viewalldrills")
